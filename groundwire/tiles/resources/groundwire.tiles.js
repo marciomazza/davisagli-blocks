4820201534d1e41a0c80dcde62a5fdf55e2ddd27
Marcio Mazza <marciomazza@gmail.com>
2012-05-28 21:10:17 -0300

    Incorporate js changes from github.com/vwc/vwc.tiles

--- groundwire/tiles/resources/groundwire.tiles.js
+++ groundwire/tiles/resources/groundwire.tiles.js
@@ -1,15 +1,24 @@
+// jQuery function
+/*global common_content_filter:false */
 jQuery(function($) {
-  $('div[data-tile]').each(function() {
+  $('div[data-tile], h1[data-tile], h2[data-tile]').each(function() {
       $(this).addClass('tile-editable');
       var href = $(this).attr('data-tile');
       var edithref = href.replace(/@@/, '@@edit-tile/');
-      $('<a class="tile-edit-link" href="' + edithref + '"><img src="pencil_icon.png" width="16" height="16"/></a>')
+      $('<a class="tile-edit-link" href="' + edithref + '"><img src="pencil_icon.png" width="16" height="16" alt="Edit Tile"/></a>')
         .appendTo($(this))
         .prepOverlay({
             subtype: 'iframe',
             config: {
-                onClose: function() { location.reload(); }
+              closeOnClick: false,
+              mask: {
+                color: '#000000',
+                opacity: 0.8
+              },
+              onClose: function() {
+                location.reload();
             }
+            },
         });
   });
   
@@ -26,5 +35,14 @@ jQuery(function($) {
           });
       }
   }
+  $(document).bind('loadInsideOverlay', function() {
+        $('textarea.mce_editable').each(function() {
+          var id = $(this).attr('id'),
+              config = new TinyMCEConfig(id);
+          //var config = new TinyMCEConfig($(this).attr('id'));
+          delete InitializedTinyMCEInstances[id];
+          config.init();
+        });
+    });
   
 });
